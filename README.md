6a49332630620ddd413393b7947d756916ee7a20
Públio Caio Pires Bispo Rodrigues <publiocaiopires@gmail.com>
2021-08-18 23:29:41 -0300

    Teste do Git-Hub

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 20115892-5_PublioCaioBispo
- Mapa Back end 1 PhP
+ Mapa Back end 1 PhP Unicesumar Aluno Públio Caio Pires Bispo Rodrigues
+ Curso: SUPERIOR DE TECNOLOGIA EM SISTEMAS PARA INTERNET
+ Matricula: 20115892-5
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="shortcut icon" href="images/Babysnoop03-Reborn-Candy.ico" type="image/x-icon">
+    <title>Pagina Inicial</title>
+</head>
+<body>
+    <!-- Pagina Inicial do Site !-->
+    <header>
+        
+        <img src="images/receitas-de-doces-Brigadeirão.jpg" alt="Bolo de Brigadeiro" height="250px">
+        <h1>TOPO DO SITE</h1>
+    </header>
+    <p>TESTE DO GIT HUB</p>
+
+
+</body>
+</html>
