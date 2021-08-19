92ad98068a7bd468912f8adfb5cc179cb48bbf79
Públio Caio Pires Bispo Rodrigues <publiocaiopires@gmail.com>
2021-08-18 23:41:38 -0300

    Criação de Paginas
    criação de paginas para começar o site

---
+++ pages/contato.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="shortcut icon" href="images/Babysnoop03-Reborn-Candy.ico" type="image/x-icon">
+    <title>pagina de contato</title>
+</head>
+<body>
+    <p>pagina de contado e localização</p>
+</body>
+</html>
