bf27d7f8fc9fc5b1100f6c966467cade247a2e17
Públio Caio Pires Bispo Rodrigues <publiocaiopires@gmail.com>
2021-08-23 09:04:23 -0300

    update

--- pages/quemsomo.php
+++ pages/quemsomo.php
@@ -5,6 +5,6 @@
  
  <p>
  	
-	Texto Aqui Quem Somos
+ Uma empresa que teve inicio nesse sonho de tocar vidas através dos doces, que busca além de trazer uma satisfação ao paladar, uma experiência unica e compartilhável. <br>Você faz parte dessa história!
  </p>
 </div>
